ecaae42856b27d494af0818ee529ed4394fd2bb5
Emmanuel <akomdoemmanuel22@gmail.com>
2026-01-31 15:25:58 +0000

    learning C# basics

---
+++ MyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Myfirst
+{
+    class Emma
+    {
+        static void Main(String[] args)
+        {
+            int number = 3;
+            Console.WriteLine(number);
+            
+        }
+    }
+}
